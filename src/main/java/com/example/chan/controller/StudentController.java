0a92254f04e5754320410e2d6a11311bb7c890bc
m83city <rostorguievv@gmail.com>
2024-09-05 19:08:41 +0300

    create connection with kafka and created topics

--- src/main/java/com/example/chan/controller/StudentController.java
+++ src/main/java/com/example/chan/controller/StudentController.java
@@ -1,8 +1,25 @@
 package com.example.chan.controller;
 
+import com.example.chan.kafka.KafkaMessageProducer;
+import com.example.chan.kafka.KafkaProducer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 
+    private KafkaProducer kafkaProducer;
+
+    private KafkaMessageProducer kafkaMessageProducer;
+
+    public StudentController(KafkaProducer kafkaProducer, KafkaMessageProducer kafkaMessageProducer) {
+        this.kafkaProducer = kafkaProducer;
+        this.kafkaMessageProducer = kafkaMessageProducer;
+    }
+
+    @PostMapping("kafka")
+    void postMessage(@RequestParam(name = "message")  String message){
+        kafkaMessageProducer.sendMessage("topic_message", message);
+    }
 }
---
+++ src/main/java/com/example/chan/kafka/KafkaMessageProducer.java
@@ -0,0 +1,17 @@
+package com.example.chan.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaMessageProducer {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage (String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+
+}
